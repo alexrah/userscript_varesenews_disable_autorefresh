571d77ceb2959540a2845bc3676021f67f34ae46
alessandro stoppato <alexrah@gmail.com>
2022-12-12 17:50:30 +0100

    version 0.2.0: fix @match add @downloadURL add @updateURL remove custom rules add logging for debug

--- main.js
+++ main.js
@@ -3,18 +3,20 @@
 // @namespace VareseNews Disable AutoRefresh
 // @description [FORK] Disable AutoRefresh is a user script to override and disable meta refresh html tag on all websites to prevent the automatic refresh or redirection.
 // @author SMed79, alexrah
-// @version 0.1.0
+// @version 0.2.0
 // @license https://creativecommons.org/licenses/by-nc-sa/4.0/
 // @icon http://i.imgur.com/ZJ9mHLO.png
 // @supportURL https://github.com/alexrah/userscript_varesenews_disable_autorefresh/issues
-// @match   https://*.varesenews.it/
-// @match   https://*.v2media.it/
-// @match   https://*.malpensanews.it/
-// @match   https://*.saronnonews.it/
-// @match   https://*.verbanonews.it/
-// @match   https://*.malpensanews.com/
+// @match   https://*.varesenews.it/*
+// @match   https://*.v2media.it/*
+// @match   https://*.malpensanews.it/*
+// @match   https://*.saronnonews.it/*
+// @match   https://*.verbanonews.it/*
+// @match   https://*.malpensanews.com/*
 // @run-at document-start
 // @grant none
+// @downloadURL https://raw.githubusercontent.com/alexrah/userscript_varesenews_disable_autorefresh/master/main.js
+// @updateURL   https://raw.githubusercontent.com/alexrah/userscript_varesenews_disable_autorefresh/master/main.js
 // ==/UserScript==
 
 /*
@@ -38,11 +40,11 @@ http://www.isthe.com/chongo/tech/comp/cgi/index.html
             timeout_ms,
             rules;
 
-        rules = [{
-            host : 'varesenews.it',
-            timeout : .5
-        }
-        ];
+        // rules = [{
+        //     host : 'varesenews.it',
+        //     timeout : .5
+        // }
+        // ];
 
         allMetas = document.getElementsByTagName('meta');
         for (var i = 0; i < allMetas.length; i++) {
@@ -50,17 +52,22 @@ http://www.isthe.com/chongo/tech/comp/cgi/index.html
 
             if (thisMeta.httpEquiv.match(/refresh/i)) {
                 if (thisMeta.content.match(/[\D]/)) {
+
+                    console.log('thisMeta.content matched%c','color: pink', thisMeta.content );
+
                     content = thisMeta.content.split(';');
                     timeout = content[0] - 1;
+
                     rules.forEach(function (rule) {
                         if (location.host.indexOf(rule.host) > -1) {
                             timeout = rule.timeout;
                             return false;
                         }
                     })
+
                     timeout_ms = (timeout > 0) ? (timeout * 1e3) : 0;
                     setTimeout(function () {
-                        console.log('Redirection stopped after ' + timeout_ms + ' ms');
+                        console.log('Redirection stopped after ' + timeout_ms + ' ms%c','color: pink');
                         window.stop();
                     }, timeout_ms);
                 }
